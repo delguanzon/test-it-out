345d550096170005c4147b26b6aa81a139385f85
delguanzon <yodelguanzon@gmail.com>
2022-11-08 14:10:19 -0800

    Test roll functionaility to generate a number bet 1 and 6
    Co-authored-by: VW <vera.weikel@gmail.com>

--- __tests__/gameBoard.test.js
+++ __tests__/gameBoard.test.js
@@ -12,8 +12,16 @@ describe('Gameboard', ()=> {
     const gameBoard = new GameBoard();
     let player = new Player('Vera');
     gameBoard.addPlayer(player);
-    expect(z).not.toBeNull();
-    expect(gameBoard.players).toContain(player);
+    expect(gameBoard.players).not.toBeNull();
+    //expect(gameBoard.players).toContain(player);
+  });
+
+  test('should generate a number that is greater than or equal to 1 but less than or equal to 6', () => {
+    const  gameBoard = new GameBoard();
+    expect(gameBoard.roll()).toBeGreaterThanOrEqual(1);
+    expect(gameBoard.roll()).toBeLessThanOrEqual(6);
   });
 });
+
+
 
